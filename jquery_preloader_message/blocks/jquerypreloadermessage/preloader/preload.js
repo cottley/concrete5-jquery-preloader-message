9b87e77aecdad693c01cb204594e4dbf5048236f
Chirstopher Ottley <dev@referencelogic>
2013-02-18 15:15:41 -0400

    Made ajax preloader php and pass in preloadbaseurl

--- jquery_preloader_message/blocks/jquerypreloadermessage/preloader/preload.js
+++ jquery_preloader_message/blocks/jquerypreloadermessage/preloader/preload.js
@@ -1,6 +1,7 @@
       function showLoader() {
+        var preloaderFinalBaseURL = preloaderBaseURL + '/preloader/preload/';
         $.colorbox({width:"400px", height:"180px", 
-          href:preloaderBaseURL + '/preloader/preload/preload.html',
+          href:preloaderFinalBaseURL + 'preload.php?preloadbaseurl=' + encodeURIComponent(preloaderFinalBaseURL),
           overlayClose:false, onLoad: function() {
             $('#cboxClose').remove();
           }}
---
+++ jquery_preloader_message/blocks/jquerypreloadermessage/preloader/preload/preload.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<table border="0" cellpadding="10px">
+<tbody>
+<tr><td><image src="<? echo $_GET["preloadbaseurl"]; ?>preloadspinner.gif" /></td><td><image src="<? echo $_GET["preloadbaseurl"]; ?>preloadloading.gif" /></td></tr>
+</tbody>
+</table>
+</body>
+</html>
